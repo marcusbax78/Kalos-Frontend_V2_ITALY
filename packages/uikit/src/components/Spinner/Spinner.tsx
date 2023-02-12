886f37a06cd0e8ce98521f6702ff5030245a3182
Kalos Protocol <kaizendex@protonmail.com>
2023-02-12 22:27:23 +1100

    spinner

--- packages/uikit/src/components/Spinner/Spinner.tsx
+++ packages/uikit/src/components/Spinner/Spinner.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 import PanIcon from "./PanIcon";
-import PancakeIcon from "./PancakeIcon";
+// import PancakeIcon from "./PancakeIcon";
 import { SpinnerProps } from "./types";
 
 const rotate = keyframes`
@@ -29,13 +29,13 @@ const Container = styled.div`
   position: relative;
 `;
 
-const RotatingPancakeIcon = styled(PancakeIcon)`
+/* const RotatingPancakeIcon = styled(PancakeIcon)`
   position: absolute;
   top: 0;
   left: 0;
   animation: ${rotate} 2s linear infinite;
   transform: translate3d(0, 0, 0);
-`;
+`; */
 
  const FloatingPanIcon = styled(PanIcon)`
   animation: ${float} 6s ease-in-out infinite;
@@ -45,7 +45,7 @@ const RotatingPancakeIcon = styled(PancakeIcon)`
 const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
   return (
     <Container>
-      <RotatingPancakeIcon width={`${size * 0.5}px`} />
+      
       <FloatingPanIcon width={`${size}px`} />
     </Container>
   );

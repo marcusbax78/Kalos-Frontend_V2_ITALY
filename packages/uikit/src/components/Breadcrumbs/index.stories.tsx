553303ec150ac01dd13ed9095837cc1f6eff7d03
Kalos Protocol <kaizendex@protonmail.com>
2023-02-13 11:14:28 +1100

    rebranding

--- packages/uikit/src/components/Breadcrumbs/index.stories.tsx
+++ packages/uikit/src/components/Breadcrumbs/index.stories.tsx
@@ -25,8 +25,8 @@ export const Default: React.FC = () => {
           <Text color="textDisabled">Crumb 2</Text>
         </BreadCrumbs>
         <BreadCrumbs>
-          <Text>PancakeSwap</Text>
-          <Text>The #1 AMM and yield farm on Binance Smart Chain.</Text>
+          <Text>KalosDeFi</Text>
+          <Text>AMM and yield farm on Binance Smart Chain.</Text>
         </BreadCrumbs>
       </Text>
     </BrowserRouter>
